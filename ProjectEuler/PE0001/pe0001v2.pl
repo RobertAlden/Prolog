de517c9022b7524daa009019579c137b0de631cc
RobertAlden <randydonst@gmail.com>
2023-01-19 17:21:22 -0600

    I made a reversable numlist, needs more work

--- ProjectEuler/PE0001/pe0001v2.pl
+++ ProjectEuler/PE0001/pe0001v2.pl
@@ -1,18 +1,10 @@
 factor(N, I, true):- I mod N #= 0.
 factor(N, I, false):- I mod N #\= 0.
-factors(N, L, R):- tfilter(factor(N), L, R).
-union(Ls, U) :- foldl(union, Ls, [], U).
 
-pe1(F, N, R):-
+pe1(Fs, N, R):-
     numlist(1, N, L0),
-    maplist([X]>>(factors(X,L0)),F,L1),
-    union(L1,L2),
-    sum_list(L2,R).
-
-pe1_a(F, N, R):-
-    numlist(1, N, L0),
-    maplist([X]>>(tfilter(factor(X),L0)),F,L1),
-    union(L1,L2),
+    maplist([X]>>(tfilter(factor(X),L0)),Fs,L1),
+    foldl(union, L1, [], L2),
     sum_list(L2,R).
 
 answer:-
--- scratch/scratchfile.pl
+++ scratch/scratchfile.pl
@@ -2,14 +2,44 @@
 reverse_([]) --> [].
 reverse_([L|Ls]) --> reverse_(Ls), [L].
 
-fib_topdown_list_cache_clpfd(N,F,Cache) :-
+fibonacci(N,F,Cache) :-
    M is N+1,
    length(Cache,M),
-   setup_constraints(Cache),
+   fibonacci(Cache),
    Cache=[F|_].
 
-setup_constraints([0]).
-setup_constraints([1,0]).
-setup_constraints([F,FA,FB|More]) :-
+fibonacci([0]).
+fibonacci([1,0]).
+fibonacci([F,FA,FB|More]) :-
    F #= FA+FB,
-   setup_constraints([FA,FB|More]).
+   fibonacci([FA,FB|More]).
+
+numlist_(X,Y,Z) :- 
+   S #= Y - X + 1,
+   S1 #= Y - X,
+   length(Z,S),
+   nth0(0, Z, X),
+   nth0(S1, Z, Y),
+   ascending_(Z).
+
+ascending_([]).
+ascending_([_]).
+ascending_([Z1,Z2|Zs]) :- 
+   succ(Z1, Z2),
+   ascending_([Z2|Zs]).
+
+is_odd(X, true) :- X mod 2 #= 1.
+is_odd(X, false) :- not(is_odd(X, true)).
+is_even(X, true) :- is_odd(X, false).
+is_even(X, false) :- is_odd(X, true).
+
+odd_list(X,Y,R) :- 
+   numlist_(X,Y,L),
+   tfilter(is_odd, L, R).
+
+
+robert(X) :- X mod 5 #= 0.
+%% is_robert(X,true) :- robert(X).
+%% is_robert(X,false) :- not(robert(X)).
+r(Pred, Arg, true) :- call(Pred, Arg).
+r(Pred, Arg, false) :- not(call(Pred, Arg)).
